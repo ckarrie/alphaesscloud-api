6ff5a1509f309df3cbc3da9c0adc0cace7e56e6c
Christian Karrié <ckarrie@users.noreply.github.com>
2023-10-07 19:11:31 +0200

    Update const.py

--- alphaesscloud_api/const.py
+++ alphaesscloud_api/const.py
@@ -1,9 +1,19 @@
-# const
+# URLs
 BASE_URL = "https://cloud.alphaess.com/api/"
 LOGIN_PATH = "Account/Login"
 GET_SETTING_PATH = "Account/GetCustomUseESSSetting"
+
+# JSON/HEADERS Keys
 JSON_KEY_ACCESS_TOKEN = "AccessToken"
 JSON_KEY_AUTHSIGNATURE = "Authsignature"
 JSON_KEY_AUTHTIMESTAMP = "Authtimestamp"
 JSON_KEY_AUTHORIZATION = "Authorization"
 
+# Cloud
+# thanks to https://bitbucket.org/vdwals/alphaesscloud2mqtt/src/master/src/main/java/de/vdw/io/alpha2mqtt/config/Constants.java
+AUTH_SIGNATURE_START = "al8e4s"
+AUTH_SIGNATURE_HASH = "LS885ZYDA95JVFQKUIUUUV7PQNODZRDZIS4ERREDS0EED8BCWSS"
+AUTH_SIGNATURE_END = "ui893ed"
+
+# Validation
+MAX_CURRENT_RANGE = range(6, 16)
