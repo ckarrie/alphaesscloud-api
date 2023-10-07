eee5a99a2b2282c3bb7a19ae4e43b6377a071902
Christian Karrié <ckarrie@users.noreply.github.com>
2023-10-07 19:08:42 +0200

    Create const.py

---
+++ alphaesscloud_api/const.py
@@ -0,0 +1,9 @@
+# const
+BASE_URL = "https://cloud.alphaess.com/api/"
+LOGIN_PATH = "Account/Login"
+GET_SETTING_PATH = "Account/GetCustomUseESSSetting"
+JSON_KEY_ACCESS_TOKEN = "AccessToken"
+JSON_KEY_AUTHSIGNATURE = "Authsignature"
+JSON_KEY_AUTHTIMESTAMP = "Authtimestamp"
+JSON_KEY_AUTHORIZATION = "Authorization"
+
